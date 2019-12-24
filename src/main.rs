4b42c1e5524424c213c10c68add5bee30f4a4d34
flyq <flyq951@gmail.com>
2019-12-24 22:13:26 +0800

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
 //! Substrate Node Template CLI library.
 
-#![warn(missing_docs)]
-#![warn(unused_extern_crates)]
+#![warn(missing_docs)]		// 编译器的 Lint 设置。缺乏文档时，在编译时打出警告。参考：http://llever.com/rustc-zh/print.html。
+#![warn(unused_extern_crates)]		// 同上。存在未使用的外部 crate 时，在编译时打出警告。
 
-mod chain_spec;
+mod chain_spec;		// 使用 chain_spec mod
 mod service;
 mod cli;
 
